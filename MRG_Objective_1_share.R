bcc6e7003553f71314eee96f4447ff4dd5c0a049
Naycari De Luna <naycarideluna@naycaris-air-6.attlocal.net>
2021-04-07 23:04:01 -0700

    worked a bit on ob1 for deaths and recovery

--- MRG_Objective_1_share.R
+++ MRG_Objective_1_share.R
@@ -18,6 +18,7 @@ View(confirmed)
 confirmed_region <- confirmed[confirmed$X1.22.20 == max(confirmed$X1.22.20), c("Province.State", "Country.Region", "X1.22.20")]
 confirmed_region
 
+
 # Aggregates Province.Region data by Country.Region and identifies
 # Country.Region with highest confirmed cases on the first day.
 confirmed_agg <- aggregate(cbind(X1.22.20) ~ Country.Region, data=confirmed, FUN=sum)
@@ -29,4 +30,10 @@ confirmed_country
 ##  Still missing syntax to include deaths and recovered and
 ##  Use if statements to see if they all match.
 
+# NDL -- Creates variable with province/state and country/region based on max deaths on 01/22/20
+deaths_origin <- deaths[which.max(deaths$X1.22.20), c("Province.State", "Country.Region")]
 
+# NDL -- Creates variable with province/state and country/region based on max recovered on 01/22/20
+recovered_origin <- recovered[which.max(recovered$X1.22.20), c("Province.State", "Country.Region")]
+
+# NDL -- These two might change as i think of how to compare the three object comparison
